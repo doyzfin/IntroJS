8cb8c692ee64d175e729df8a0148daa3bb6ca144
idoy <alfinramadhan97@gmail.com>
2021-03-21 13:39:03 +0700

    TUGAS

---
+++ Tugas/task4.js
@@ -0,0 +1,23 @@
+let data = {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: 
+    {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    }
+
+//Spread Operator
+const newData = {...data, name : "Alfin", email : "alfinramadhan97@gmail.com", hobby : "Gaming"}
+console.log(newData)
+
+//Destucturing
+const {street, city} = data.address
+console.log(street,city)
